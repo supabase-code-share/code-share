7f510365216f1fd29065b78fa039b2bd84868712
Nahom Haile <nahom9688@gmail.com>
2022-08-14 20:49:45 -0400

    genric login page without styles

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,17 +1,16 @@
 const Login = () => {
   return (
     <div>
-        <h1>Create and Account</h1>
+        <h1>Login</h1>
 
         <div>
-        <form>
-            <input type="email" placeholder='Email Addres' className='block text-sm py-3 px-4 rounded-lg w-full border outline-none'/>
-            <input type="password" placeholder="Password" className='block text-sm py-3 px-4 rounded-lg w-full border outline-none'/>
+            <form>
+                <input type="email" placeholder='Email Addres' className=''/>
+                <input type="password" placeholder="Password" className=''/>
 
-            <button className='py-3 w-64 text-xl text-white bg-blue-400 rounded-2xl'>Create Account</button>
+                <button className=''>Login</button>
 
-            <p className='mt-4 text-sm'>Already Have An Account? <span className='underline cursor-pointer'> Sign In</span></p>
-        </form>
+            </form>
         </div>
 
     </div>
