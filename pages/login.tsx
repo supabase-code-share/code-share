312de4f1e2d390eaf8eb022106f8684deb39e576
Nahom Haile <nahom9688@gmail.com>
2022-08-15 19:25:04 -0400

    testing vercel deployment changes

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,18 +1,12 @@
+
 const Login = () => {
   return (
-    <div>
-        <h1>Login</h1>
-
-        <div>
-            <form>
-                <input type="email" placeholder='Email Addres' className=''/>
-                <input type="password" placeholder="Password" className=''/>
-
-                <button className=''>Login</button>
-
-            </form>
-        </div>
-
+    <div className="m-20 border border-sky-500 text-center">
+        <form className="grid grid-cols-1 divide-y">
+            <input type="email" placeholder='Email Addres' className=''/>
+            <input type="password" placeholder="Password" className=''/>
+            <button className=''>Login</button>
+        </form>
     </div>
   )
 }
