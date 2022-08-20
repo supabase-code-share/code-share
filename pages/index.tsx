52be1c0fe0758346987b3d581ae39957224d3af9
Nahom Haile <nahom9688@gmail.com>
2022-08-20 19:25:52 -0400

    create account and login done

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,20 @@ import { Auth } from '@supabase/ui';
 import type { NextPage } from "next";
 import Link from "next/link";
 import Layout from "../components/Layout";
-import { useAuth } from '../lib/auth';
+import { useAuth, VIEWS } from '../lib/auth';
 import { supabase } from "../utils/supabaseClient";
 
 const Home: NextPage = () => {
 
 	const { user, view, signOut } = useAuth();
+
+	if (view === VIEWS.UPDATE_PASSWORD) {
+		return (
+		  <Layout>
+			<Auth.UpdatePassword supabaseClient={supabase} />
+		  </Layout>
+		);
+	  }
 
 	return (
 		<Layout>
@@ -17,6 +25,9 @@ const Home: NextPage = () => {
 				<code className="highlight">{user.role}</code>
 				<Link href="/profile">
 					<a className="button">Go to Profile</a>
+				</Link>
+				<Link href="/upload">
+					<a className="button">Create a new snippet</a>
 				</Link>
 				<button type="button" className="button-inverse" onClick={signOut}>
 					Sign Out
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,15 +1,34 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import { supabase } from "../utils/supabaseClient";
 
-export default function Profile() {
+export default function Profile({ user }) {
   return (
     <Layout>
-      <h2>User Profile</h2>
-      <div className="heading">Last Signed In:</div>
-      <code className="highlight">N/A</code>
-      <Link href="/">
-        <a className="button">Go Home</a>
-      </Link>
+        <p className="my-4 text-lg">Email: <span className='font-bold'>{user.email}</span></p>
+        <p className="my-4 text-lg">
+            Last Signed In: 
+            <span className="pl-2 font-bold">
+            {new Date(user.last_sign_in_at).toLocaleString()}
+            </span>
+        </p>
+        <div className="inline-flex flex-col">
+            <Link href="/">
+            <a className="button mt-8">Go Home</a>
+            </Link>
+        </div>
     </Layout>
   );
 }
+
+
+export async function getServerSideProps({ req, res }) {
+    const { user } = await supabase.auth.api.getUserByCookie(req);
+  
+    if (!user) {
+      console.log('Please login.');
+      return { props: {}, redirect: { destination: '/', permanent: false } };
+    }
+  
+    return { props: { user } };
+  }
