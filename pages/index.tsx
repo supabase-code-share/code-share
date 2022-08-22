a7ae169d38561f5e1afe3cc3a8f3188474111a62
Nahom Haile <nahom9688@gmail.com>
2022-08-21 22:02:25 -0400

    code for like snippet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { Auth } from "@supabase/ui";
 import { GetStaticProps, NextPage } from "next";
 import Image from 'next/image';
+import { useState } from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Layout from "../components/Layout";
 import { useAuth } from "../lib/auth";
-import Comment from '../public/comment.svg';
 import CheckMark from '../public/like.svg';
 import { supabase } from "../utils/supabaseClient";
 
@@ -24,27 +24,58 @@ type props = {
 const IndexPage: NextPage<props> = (props) => {
 	const { snippets } = props;
 	const { user, view } = useAuth();
+	const [like, setLike] = useState(false)
+	const [dislike, setdilike] = useState(false)
+
+	// let readSnippet = async (_snippet_id) => {	
+
+	// 	let { data, error } = await supabase
+	// 									.from('like')
+	// 									.select('*')
+	// 									.eq('snippet_id', _snippet_id)
+
+
+	// 	return data;
+
+	// }
+
+
+	// let likeSnippet = async (_snippet_id) => {
+
+	// 	let checkIfTheyLiked = await readSnippet(_snippet_id)
+
+	// 	setLike(!like)
+
+	// 	if (checkIfTheyLiked?.length === 0){
+	// 		const { data, error } = await supabase.from("like").insert({
+	// 			snippet_id: _snippet_id,
+	// 			user_id: user.id
+	// 		});
+	// 	} 
+	// }
+
+
+	let handleLike = () => {
+		setLike()
+	}
 
 	return (
 		<div>
 			{user ? (
 				<>
 					<Header/>
-						<div className="p-10 grid grid-cols-2 gap-15">
+						<div className="p-10 grid grid-cols-2 gap-16 gap-x-16">
 							{snippets?.map((snippet) => (
 								<div className="flex flex-col bg-gray-100 w-max p-5 relative" key={snippet.id}>
 									<span className="text-center text-lg">{snippet.title}</span>
 									<code className="bg-gray-400 w-128 text-white p-4 rounded-md">{snippet.code_block}</code>
-									{snippet.description && <span>{snippet.description}</span>}
-
-									{/* https://icons.getbootstrap.com/icons/chat-right-text/ */}
-									<div className="absolute -bottom-1 right-0">
-										<Image src={Comment} width='30' height='30'/>
-									</div>		
+									{snippet.description && <span>{snippet.description}</span>}	
 									
 									{/* https://icons.getbootstrap.com/icons/check-circle/ */}
-									<div className="absolute bottom-0 right-9">
-										<Image src={CheckMark} width='27' height='27'/>
+									<div className="absolute bottom-0 right-0" >
+										<button type="button" onClick={() => handleLike} id={"snippet" + snippet.id} disabled={like}>
+											<Image src={CheckMark} width='27' height='27'/>
+										</button>										
 									</div>					
 				
 								</div>
@@ -63,7 +94,7 @@ const IndexPage: NextPage<props> = (props) => {
 };
 
 export const getStaticProps: GetStaticProps<props> = async (context) => {
-	const query = await supabase.from<Snippet>("Snippet").select();
+	const query = await supabase.from<Snippet>("snippet").select();
 	const snippets = query.data;
 
 	return {
